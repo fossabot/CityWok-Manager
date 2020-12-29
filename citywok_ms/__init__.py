a26358172e7f3b2b0f965e76796d7926cb153161
Henrique Lin <henrique.lin99@gmail.com>
2020-12-29 20:43:14 +0000

    Initial Commit

---
+++ citywok_ms/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+def create_app():
+    # create the app instance
+    app = Flask(__name__)
+    
+    # init extensions
+    # TODO:
+    
+    with app.app_context():
+        # imports
+        # TODO:
+        
+        # blueprints
+        # TODO:
+        
+        return app
+    
