ad429731fa24aea1c339fb3e61ad776e66dce48a
Henrique Lin <henrique.lin99@gmail.com>
2020-12-30 17:19:12 +0000

    Initialize SQLAlchemy

--- citywok_ms/__init__.py
+++ citywok_ms/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from citywok_ms.config import Config
+
+db = SQLAlchemy()
 
 
 def create_app(config_class=Config):
@@ -8,14 +11,13 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     # init extensions
-    # TODO:
-    
+    db.init_app(app)
+
     with app.app_context():
         # imports
         # TODO:
-        
+
         # blueprints
         # TODO:
-        
+
         return app
-    
