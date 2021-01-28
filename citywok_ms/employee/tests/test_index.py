2c4ced6d7c134ab7af1f95de6b1a5edd396a8c50
Henrique Lin <henrique.lin99@gmail.com>
2021-01-28 15:58:53 +0000

    Add more tests for employee/index

--- citywok_ms/employee/tests/test_index.py
+++ citywok_ms/employee/tests/test_index.py
@@ -8,6 +8,13 @@ def test_get(test_client):
     assert b'New' in response.data
 
 
+def test_new_btn(test_client):
+    response = test_client.get('/employee/')
+    assert response.status_code == 200
+    assert b'/employee/new' in response.data
+    assert b'New' in response.data
+
+
 def test_employee_data(test_client, test_employees):
     response = test_client.get('/employee/')
     assert b'TEST_1' in response.data
